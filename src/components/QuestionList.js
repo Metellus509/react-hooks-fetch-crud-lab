d1b46162ad38965c2d2787dcd46c2c88582f08f3
Metellus <metellusjunior56@gmail.com>
2025-07-07 15:17:10 -0400

    complete lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,36 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+  const [qitem, setQitem] = useState([])
+  useEffect(()=>{
+      fetch('http://localhost:4000/questions')
+      .then(r=>r.json())
+      .then(data=>setQitem(data))
+  },[])
+
+  function deleteAction(id){
+    console.log(id)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+
+    setQitem((item)=>item.filter((question)=>question.id != id))
+    console.log(qitem)
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+      <ul>
+      {qitem.map((question) => (
+      <QuestionItem key={question.id} question={question} handleDelete={deleteAction} />
+      ))}
+      </ul>
+    </section> 
+
   );
 }
 
